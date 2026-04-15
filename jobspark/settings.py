f13a0096803e600b1874f2b20c9e07aa8652c5af
Ashwini Pawde <ashwinipawde1@gmail.com>
2026-04-15 12:00:22 +0530

    fix deployment requirements

--- jobspark/settings.py
+++ jobspark/settings.py
@@ -114,11 +114,11 @@ MESSAGE_TAGS = {
 }
 
 # Manually added!
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
 # STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
+#     BASE_DIR / 'static'
 # ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
